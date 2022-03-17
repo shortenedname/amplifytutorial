99c9e71169e6213104d66043770bc211e1fca9a2
Noah Abrams <nabrams@Noahs-MacBook-Pro.local>
2022-03-17 15:28:33 -0400

    added authentication?

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { Amplify } from 'aws-amplify';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <h2> Is there anything that shows up?</h2>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
